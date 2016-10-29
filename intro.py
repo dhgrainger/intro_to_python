9d217c9b6f26a15f88572500e60650bd0f26e0cd
Doug Grainger <dhgrainger@gmail.com>
2016-10-29 14:42:58 -0400

    start learning some python

---
+++ intro.py
@@ -0,0 +1,8 @@
+class Program():
+  def __init__(self, *args, **kwargs):
+
+      self.lang = input("What Langugage?: ")
+      self.version = float(input("What Version?: "))
+      self.skill = input("What skill level?: ")
+
+p1 = Program()
